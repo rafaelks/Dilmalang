0cbbe1ceaac6d15d71ade79cfefa8a2299948843
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-11-10 18:45:23 -0200

    Add tests

--- test/transpiler.js
+++ test/transpiler.js
@@ -5,7 +5,7 @@ import assert from 'assert';
 function compare(source, target) {
 	const compiler = new Compiler(source);
 	compiler.compiled = compiler.compiled.replace(/\n$/, '');
-	assert.equal(compiler.compiled, target);
+	assert.equal(target, compiler.compiled);
 }
 
 describe('Transpiler', function() {
@@ -29,6 +29,10 @@ describe('Transpiler', function() {
 		compare('politico salario = true.,', 'var salario = true;');
 
 		compare('politico salario = false.,', 'var salario = false;');
+	});
+
+	it('should transpile a attribution of an expression', function() {
+		compare('salario = salario + i.,', 'salario = salario + i');
 	});
 
 	it('should transpile a print with no value', function() {
@@ -56,7 +60,7 @@ describe('Transpiler', function() {
 	});
 
 	it('should transpile a empty if with empty else', function() {
-		compare('porque (salario < 100000) {} casoContrario {}', 'if (salario < 100000) {} else {\n}');
+		compare('porque (salario < 100000) {} casoContrario {}', 'if (salario < 100000) {\n}');
 	});
 
 	it('should transpile expression `1 + 1`', function() {
@@ -74,4 +78,43 @@ describe('Transpiler', function() {
 	it('should transpile expression `4 / 2 + 1 * 3 - 2`', function() {
 		compare('4 / 2 + 1 * 3 - 2', '4 / 2 + 1 * 3 - 2');
 	});
+
+	it('should transpile example 1', function() {
+		const source = `politico salario = 100000.,
+
+euViVoceVeja (i = 0., i < 10000., i++) {
+	salario = salario + i.,
+
+	// Outro comentário
+	porque (salario < 100000) {
+		midiaGolpista("Número abaixo de 100.000: " + salario).,
+
+		porque (salario > 10000000) {
+			midiaGolpista("Número chegou no máximo de 10.000.000: " + salario).,
+			pareiDeVer.,
+		}
+	} casoContrario {
+		midiaGolpista("Número maior do que 100.000: " + salario).,
+		euJaVi.,
+	}
+}`;
+
+		const target = `var salario = 100000;
+for (i = 0; i < 10000; i++) {
+    salario = salario + i;
+    if (salario < 100000) {
+        console.log("Número abaixo de 100.000: " + salario);
+        if (salario > 10000000) {
+            console.log("Número chegou no máximo de 10.000.000: " + salario);
+            break;
+        }
+    } else {
+        console.log("Número maior do que 100.000: " + salario);
+        continue;
+    }
+}`;
+		compare(source, target);
+	});
+
+
 });
